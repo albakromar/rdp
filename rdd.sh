2e13958a138b16fce48b37293ebe67d8bec430c9
albakromar <93453055+albakromar@users.noreply.github.com>
2021-11-11 15:18:41 -0800

    Update rdd.sh

--- rdd.sh
+++ rdd.sh
@@ -1,15 +1,26 @@
-#!/bin/bash
-#
-#Vars
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip.1
+
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &
-sudo apt-get update 
-sudo apt install firefox -y
-sudo apt install -y xrdp
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
+echo "===================================="
+echo "Wait 2 Minutes"
+echo "This is only for education"
+echo "Other interesting tutorials visit akuh.net"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
+sudo apt install firefox -y > /dev/null 2>&1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
